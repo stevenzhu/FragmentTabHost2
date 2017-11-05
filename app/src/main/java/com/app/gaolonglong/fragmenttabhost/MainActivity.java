d40e2a8e0f374eac1f13f0173c173ea2c0862f08
stevenzhu <qq1204500708@163.com>
2017-11-05 11:41:11 +0800

    ddfdfdf

--- app/src/main/java/com/app/gaolonglong/fragmenttabhost/MainActivity.java
+++ app/src/main/java/com/app/gaolonglong/fragmenttabhost/MainActivity.java
@@ -2,28 +2,25 @@ package com.app.gaolonglong.fragmenttabhost;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TabHost;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,View.OnTouchListener,TabHost.OnTabChangeListener{
 
     private FragmentTabHost mTabHost;
     private CustomViewPager mViewPager;
@@ -31,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout sendTagLayout;
     private Class mClass[] = {HomeFragment.class, ReportFragment.class, null,MessageFragment.class, MineFragment.class};
     private Fragment mFragment[] = {new HomeFragment(), new ReportFragment(),null, new MessageFragment(), new MineFragment()};
-    private String mTitles[] = {"首页", "报表", "消息", "我的"};
+    private String mTitles[] = {"搭伴", "发现", "消息", "我的"};
 
     private int mImages[] = {
             R.drawable.tab_home,
@@ -40,20 +37,32 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.tab_mine
     };
 
+    private View mPanelView;
+    private View mCloseButton;
+    private View mIdeaButton;
+    private View mPhotoButton;
+    private View mWeiboButton;
+    private View mLbsButton;
+    private View mReviewButton;
+    private View mMoreButton;
+
+    private Animation mButtonInAnimation;
+    private Animation mButtonOutAnimation;
+    private Animation mButtonScaleLargeAnimation;
+    private Animation mButtonScaleSmallAnimation;
+    private Animation mCloseRotateAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         init();
     }
 
     private void init() {
         initView();
         initEvent();
+        initAnimation();
     }
 
     private void initView() {
@@ -74,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
                 mTabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.WHITE);
 
         }
-        mViewPager.setAnimation(null);
         mViewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
@@ -86,6 +94,19 @@ public class MainActivity extends AppCompatActivity {
                 return mFragmentList.size();
             }
         });
+
+
+        mPanelView = findViewById(R.id.panel);
+        mCloseButton = findViewById(R.id.close);
+        mIdeaButton = findViewById(R.id.idea_btn);
+        mPhotoButton = findViewById(R.id.photo_btn);
+        mWeiboButton = findViewById(R.id.weibo_btn);
+        mLbsButton = findViewById(R.id.lbs_btn);
+        mReviewButton = findViewById(R.id.review_btn);
+        mMoreButton = findViewById(R.id.more_btn);
+
+
+
     }
 
     private View getTabView(int index) {
@@ -104,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
             title.setText(mTitles[1]);
         }else if(index==2){
             view = LayoutInflater.from(this).inflate(R.layout.tab_center_item, null);
-            ImageView image = (ImageView) view.findViewById(R.id.image);
+            //ImageView image = (ImageView) view.findViewById(R.id.image);
             //TextView title = (TextView) view.findViewById(R.id.title);
             //image.setImageResource(mImages[1]);
             //title.setText(mTitles[1]);
@@ -129,33 +150,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initEvent() {
-        sendTagLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"tabId",Toast.LENGTH_SHORT).show();
-            }
-        });
-        mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
-            @Override
-            public void onTabChanged(String tabId) {
-                if(!tabId.equals("2")){
-                    if(tabId.equals("0")){
-                        mViewPager.setCurrentItem(0);
-                    }else if(tabId.equals("1")){
-                        mViewPager.setCurrentItem(1);
-                    }else if(tabId.equals("3")){
-                        mViewPager.setCurrentItem(2);
-                    }else if(tabId.equals("4")){
-                        mViewPager.setCurrentItem(3);
-                    }
+        sendTagLayout.setOnClickListener(this);
+        mCloseButton.setOnClickListener(this);
 
-                }else{
-
-                }
-                Toast.makeText(MainActivity.this,""+tabId,Toast.LENGTH_SHORT).show();
-
-            }
-        });
+        mIdeaButton.setOnTouchListener(this);
+        mPhotoButton.setOnTouchListener(this);
+        mWeiboButton.setOnTouchListener(this);
+        mLbsButton.setOnTouchListener(this);
+        mReviewButton.setOnTouchListener(this);
+        mMoreButton.setOnTouchListener(this);
+        mTabHost.setOnTabChangedListener(this);
 
         mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -185,25 +189,116 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sendTagLayout:// 添加按钮
+                openPanelView();
+                break;
+            case R.id.close:// 关闭按钮
+                closePanelView();
+                break;
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public boolean onTouch(final View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                // 手指按下，按钮执行放大动画
+                v.startAnimation(mButtonScaleLargeAnimation);
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                // 手指移开，按钮执行缩小动画
+                v.startAnimation(mButtonScaleSmallAnimation);
+                v.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 缩小动画执行完毕后，将按钮的动画清除。这里的150毫秒是缩小动画的执行时间。
+                        v.clearAnimation();
+                    }
+                }, 150);
+                break;
         }
+        return true;
+    }
 
-        return super.onOptionsItemSelected(item);
+
+    // 初始化动画
+    private void initAnimation() {
+        mButtonInAnimation = AnimationUtils.loadAnimation(this, R.anim.button_in);
+        mButtonOutAnimation = AnimationUtils.loadAnimation(this, R.anim.button_out);
+        mButtonScaleLargeAnimation = AnimationUtils.loadAnimation(this, R.anim.button_scale_to_large);
+        mButtonScaleSmallAnimation = AnimationUtils.loadAnimation(this, R.anim.button_scale_to_small);
+        mCloseRotateAnimation = AnimationUtils.loadAnimation(this, R.anim.close_rotate);
+
+        mButtonOutAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // 6个按钮的退出动画执行完毕后，将面板隐藏
+                mPanelView.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+    }
+
+    // 打开面板视图
+    private void openPanelView() {
+        mPanelView.setVisibility(View.VISIBLE);
+
+        mIdeaButton.startAnimation(mButtonInAnimation);
+        mPhotoButton.startAnimation(mButtonInAnimation);
+        mWeiboButton.startAnimation(mButtonInAnimation);
+        mLbsButton.startAnimation(mButtonInAnimation);
+        mReviewButton.startAnimation(mButtonInAnimation);
+        mMoreButton.startAnimation(mButtonInAnimation);
+
+        mCloseButton.startAnimation(mCloseRotateAnimation);
+    }
+
+    // 关闭面板视图
+    private void closePanelView() {
+        // 给6个按钮添加退出动画
+        mIdeaButton.startAnimation(mButtonOutAnimation);
+        mPhotoButton.startAnimation(mButtonOutAnimation);
+        mWeiboButton.startAnimation(mButtonOutAnimation);
+        mLbsButton.startAnimation(mButtonOutAnimation);
+        mReviewButton.startAnimation(mButtonOutAnimation);
+        mMoreButton.startAnimation(mButtonOutAnimation);
+    }
+    @Override
+    public void onBackPressed() {
+        if (mPanelView.getVisibility() == View.VISIBLE) {
+            closePanelView();
+            return;
+        }
+        super.onBackPressed();
+    }
+
+
+    @Override
+    public void onTabChanged(String tabId) {
+        if(!tabId.equals("2")){
+            if(tabId.equals("0")){
+                mViewPager.setCurrentItem(0);
+            }else if(tabId.equals("1")){
+                mViewPager.setCurrentItem(1);
+            }else if(tabId.equals("3")){
+                mViewPager.setCurrentItem(2);
+            }else if(tabId.equals("4")){
+                mViewPager.setCurrentItem(3);
+            }
+
+        }
     }
 }
